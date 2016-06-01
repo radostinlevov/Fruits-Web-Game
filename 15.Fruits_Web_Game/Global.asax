f14aae02de22194c0cab854a5c1b36ace3385635
Radostin Levov <radostin.levov@gmail.com>
2016-06-01 23:18:42 +0300

    Added some files

---
+++ 15.Fruits_Web_Game/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_15.Fruits_Web_Game.MvcApplication" Language="C#" %>
---
+++ 15.Fruits_Web_Game/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_15.Fruits_Web_Game.Startup))]
+namespace _15.Fruits_Web_Game
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
